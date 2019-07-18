968306140d3af71ad18c7496fd79ec97af1dcddf
Sandhini Agarwal <sagarwal@Sandhini-Agarwal-MBP-13.local>
2019-07-18 13:52:59 -0400

    Files

---
+++ api/routes/test1.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express.Router();
+
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+const sheetLink = '17WmUfU3AQNcbdf2bxVg9dK4pi9KzBQz5qIggw67L7t0'
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+const decidedRange = 'B:G'
+// Load client secrets from a local file.
+
+app.get("/", function(req, res, next){
+  //res.send("hyoiuou")
+
+  // Authorization
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), datapull);
+  });
+
+  // Callback function pulling data
+  function datapull(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+
+  // Pulling the data from the specified spreadsheet and the specified range
+  var result = sheets.spreadsheets.values.get({
+    // (1) Changed spreadsheet ID
+    spreadsheetId: sheetLink,
+    // (2) Changed the range of data being pulled
+    range: decidedRange,
+  }, (err, response)=>{
+    if (err) return console.log('The API returned an error: ' + err);
+
+    // (3) Setting data for daily tracking
+    const rows = response.data.values;
+    console.log(rows)
+    // (4) Rendering the page and passing the rows data in
+    res.send(rows)
+  });
+  }
+})
+
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+module.exports = app;
---
+++ client/src/App.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from '../node_modules/history';
+
+
+
+import "./App.css";
+var url = "http://localhost:9500/testAPI"
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmitY = this.handleSubmitY.bind(this);
+        this.state = { apiResponse: "" , peopleList: [""], param: "", form: "Name"};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFormPick = this.handleFormPick.bind(this);
+        const button = document.querySelector('#submitB')
+        const input = document.querySelector('#myText')
+        const form = document.querySelector('#myList')
+        form.addEventListener('change', this.handleFormPick)
+        input.addEventListener('change', this.handleChange)
+        button.addEventListener('click', this.handleSubmitY)
+    }
+
+    handleSubmitY(event) {
+      console.log('A name was submitted: ' + this.state.param + " "+this.state.form);
+      url = "http://localhost:9500/con/"
+      url += this.state.form+"/"+this.state.param
+      console.log(url)
+      event.preventDefault();
+      this.componentDidMount()
+  }
+
+  handleFormPick(event){
+    this.setState({form: event.target.value});
+  }
+
+  handleChange(event) {
+    this.setState({param: event.target.value});
+  }
+    callAPI(url) {
+        fetch(url)
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res },function() {
+              this.creatList()
+            } ))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI(url);
+    }
+
+    creatList(){
+      var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&*+\.\/;<=>?@\[\]^_`{|}~]/g;
+      var str = (this.state.apiResponse)
+      var peopleList = str.split('],[')
+      var finList = []
+      var paramList = []
+      var firstList = peopleList[0]
+      firstList = firstList.split('","')
+      for (var j = 0; j < firstList.length; j++){
+        paramList[j] = firstList[j].replace(punctRE, '').trim()
+     }
+     console.log(paramList)
+      for (var i = 1; i < peopleList.length; i++) {
+        var tempList = peopleList[i].split('","')
+        var tempDict = {}
+        for (var j = 0; j < tempList.length; j++){
+          tempList[j] = tempList[j].replace(punctRE, '').trim()
+         tempDict[paramList[j]] = tempList[j]
+       }
+        finList.push(tempDict)
+      }
+      console.log(finList)
+      this.setState({ peopleList: finList });
+    }
+
+    _renderObject(){
+      const ob = this.state.peopleList
+      return Object.entries(ob).map(([key, value], i) => {
+        return (
+          <tr id = "childTable">
+          <td scope="row">{value.Name}</td>
+          <td>{value.Position}</td>
+          <td>{value.Team}</td>
+          <td>{value.Projects}</td>
+          <td>{value.Manager}</td>
+          <td>{value.Oversees}</td>
+          </tr>
+            //<b>Position:</b> {value.Position}<br/>
+          //  <b>Projects:</b> {value.Projects}<br/>
+          //  <b>Manager:</b> {value.Manager}<br/>
+          //  <b>Oversees: </b> {value.Oversees}<br/>
+
+        )
+      })
+    }
+
+    render() {
+      return (
+    <table  id = "parentP">
+    <tbody>
+    <tr id = "headTable">
+    <th scope = "row">Name</th>
+    <th>Position</th>
+    <th>Team</th>
+    <th>Projects</th>
+    <th>Manager</th>
+    <th>Oversees</th>
+    </tr>
+    {this._renderObject()}
+     </tbody>
+     </table>
+    		)
+    }
+}
+
+export default App;
