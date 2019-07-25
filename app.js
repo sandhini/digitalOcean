f29e7d39b814389ac847260151118c22ac105851
Sandhini Agarwal <sagarwal@Sandhini-Agarwal-MBP-13.local>
2019-07-25 15:42:05 -0400

    the

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ if (process.env.NODE_ENV === 'production') {
   console.log("uygug")
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  //app.get('*', function(req, res) {
+  //  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//  });
 }
 app.use('/', testAPIRouter);
 app.use('/users', customPage);
