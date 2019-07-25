5a451dbe88a59d0263a78e1b76cc0058afd6478d
Sandhini Agarwal <sagarwal@Sandhini-Agarwal-MBP-13.local>
2019-07-25 11:19:18 -0400

    try new

--- app.js
+++ app.js
@@ -25,11 +25,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', testAPIRouter);
 app.use('/users', customPage);
 app.use("/testAPI", testAPIRouter);
-app.use('/con', customPage);
-
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
